367d91f226d0a099230f378448b80518ff9b79cc
bylee5 <chchch888@naver.com>
2023-04-04 10:35:10 +0900

    네비게이션바 변경

--- server/models.py
+++ server/models.py
@@ -3,11 +3,15 @@ from datetime import datetime
 
 class Server(models.Model):
 	svr = models.CharField(max_length=100) # DB서버
+	ver = models.CharField(max_length=100) # DB버전
 	usg = models.CharField(max_length=100) # 용도
+	vip = models.CharField(max_length=100) # 가상 ip
+	pri_ip = models.CharField(max_length=100) # 사설 ip
+	pub_ip = models.CharField(max_length=100) # 공용 ip
 	port1 = models.IntegerField() # 포트 번호
-	ip1 = models.CharField(max_length=100) # ip1
-	ip2 = models.CharField(max_length=100) # ip2
 	priority = models.CharField(max_length=100) # 분류
+	manager1 = models.CharField(max_length=100) # 정
+	manager2 = models.CharField(max_length=100) # 부
 	created_at = models.DateTimeField(default=datetime.now) # 생성일
 	updated_at = models.DateTimeField(default=datetime.now) # 변경일
 	
--- server/views.py
+++ server/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def server(request):
     page = request.GET.get('page', '1')  # 페이지
-    query = "SELECT * FROM server_list"
+    query = "SELECT * FROM server_list order by id desc"
     server_list = Server.objects.raw(query)
     paginator = Paginator(server_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
