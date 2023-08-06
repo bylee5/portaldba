06799bc1ce9211527dab66856704a55cdb99a8dc
bylee5 <chchch888@naver.com>
2023-08-06 16:30:48 +0900

    서버관리 서버삭제이력 구현

--- server/urls.py
+++ server/urls.py
@@ -6,7 +6,6 @@ app_name = 'server'
 urlpatterns = [
     # ex: /server/
     path('', views.server, name='server'),
-    path('server_remove', views.server_remove, name='server_remove'),
     path('select', views.server_select, name='server_select'),
     path('dummy', views.server_dummy, name='server_dummy'),
     path('insert', views.server_insert, name='server_insert'),
@@ -14,4 +13,7 @@ urlpatterns = [
     path('update', views.server_update, name='server_update'),
     path('multi_dml', views.server_multi_dml, name='server_multi_dml'),
     
+    # 서버 삭제이력
+    path('server_remove', views.server_remove, name='server_remove'),
+    path('server_remove_select', views.server_remove_select, name='server_remove_select'),
 ]
--- server/views.py
+++ server/views.py
@@ -639,6 +639,76 @@ def server_multi_dml(request):
 
 @login_required
 def server_remove(request):
-    server_svr_list = Server.objects.all()
+    server_svr_list = Server.objects.all().filter(delete_yn='Y')
     context = {'server_svr_list': server_svr_list}
     return render(request, 'server/server_remove.html', context)
+
+@login_required
+def server_remove_select(request):
+    if request.method == 'POST':
+        dbtype = request.POST.get('s_dbtype')
+        dbenv = request.POST.get('s_dbenv')
+        dbservice = request.POST.get('s_dbservice')
+        dbsvr = request.POST.get('s_dbsvr')
+        dbver = request.POST.get('s_dbver')
+        audit_yn = request.POST.get('s_audit_yn')
+        url = request.POST.get('s_url')
+        callmorepostFlag = 'true'
+
+        if dbsvr == '':
+            # 전체 서버 검색
+            server_list = Server.objects.filter(
+                dbtype__contains=dbtype,
+                dbenv__contains=dbenv,
+                dbservice__contains=dbservice,
+                dbver__contains=dbver,
+                audit_yn__contains=audit_yn,
+                url__contains=url,
+                delete_yn='Y'
+            ).order_by('-deleted_at')
+        else:
+            # 특정 서버 검색
+            server_list = Server.objects.filter(
+                dbtype__contains=dbtype,
+                dbenv__contains=dbenv,
+                dbservice__contains=dbservice,
+                dbsvr=dbsvr,
+                dbver__contains=dbver,
+                audit_yn__contains=audit_yn,
+                url__contains=url,
+                delete_yn='Y'
+            ).order_by('-deleted_at')
+
+        page = int(request.POST.get('page'))
+        total_count = server_list.count()
+        page_max = math.ceil(total_count / 35)
+        paginator = Paginator(server_list, page * 35)
+
+        try:
+            if int(page) >= page_max : # 마지막 페이지 멈춤 구현
+                server_list = paginator.get_page(1)
+                callmorepostFlag = 'false'
+            else:
+                server_list = paginator.get_page(1)
+        except PageNotAnInteger:
+            server_list = paginator.get_page(1)
+        except EmptyPage:
+            server_list = paginator.get_page(paginator.num_pages)
+
+        context = {
+            'dbtype': dbtype,
+            'dbenv': dbenv,
+            'dbservice': dbservice,
+            'dbsvr': dbsvr,
+            'dbver': dbver,
+            'audit_yn': audit_yn,
+            'url': url,
+            'server_list': server_list,
+            'total_count': total_count, 'callmorepostFlag': callmorepostFlag,
+            'page_max': page_max
+        }
+
+        return render(request, 'server/server_remove_select.html', context)
+
+    else:
+        return render(request, 'server/server_remove.html')
