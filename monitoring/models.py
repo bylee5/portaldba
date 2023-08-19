1497bea77e4501502f497e69db5a9b36012dbc1b
bylee5 <chchch888@naver.com>
2023-08-20 07:55:47 +0900

    모니터링관리 메뉴 수정

--- monitoring/models.py
+++ monitoring/models.py
@@ -9,3 +9,21 @@ class Monitoring(models.Model):
 
 	def __str__(self):
 		return self.svr
+
+class Monitoring(models.Model):
+	svr = models.CharField(max_length=100) # DB서버
+
+	class Meta:
+		db_table = 'monitoring'
+
+	def __str__(self):
+		return self.svr
+
+class Monitoring(models.Model):
+	svr = models.CharField(max_length=100) # DB서버
+
+	class Meta:
+		db_table = 'monitoring'
+
+	def __str__(self):
+		return self.svr
