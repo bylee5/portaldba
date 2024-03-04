656b08414759dea80f8aa533711f09f410b9171f
bylee5 <chchch888@naver.com>
2024-03-04 22:45:18 +0900

    주석 추가

--- account/apps.py
+++ account/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+# default_auto_field 설정을 통해 자동으로 증가하는 id 컬럼(PK)을 추가함
 class AccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'account'
