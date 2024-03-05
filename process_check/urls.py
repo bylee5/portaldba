1931b822a8d48ee99f0fa37d65233e10aa14dc03
bylee5 <chchch888@naver.com>
2024-03-06 00:06:37 +0900

    잡 기능 추가

--- process_check/urls.py
+++ process_check/urls.py
@@ -12,4 +12,10 @@ urlpatterns = [
     path('server_list_delete_svr_use_yn_ajax', views.server_list_delete_svr_use_yn_ajax, name='server_list_delete_svr_use_yn_ajax'),
     path('server_list_update_svr_use_yn_ajax', views.server_list_update_svr_use_yn_ajax, name='server_list_update_svr_use_yn_ajax'),
 
+    path('server_job_list', views.server_job_list, name='server_job_list'),
+    path('server_job_list_left_ajax', views.server_job_list_left_ajax, name='server_job_list_left_ajax'),
+    path('server_job_list_right_ajax', views.server_job_list_right_ajax, name='server_job_list_right_ajax'),
+    path('server_job_list_update_job_use_yn_ajax', views.server_job_list_update_job_use_yn_ajax, name='server_job_list_update_job_use_yn_ajax'),
+    path('server_job_list_delete_job_use_yn_ajax', views.server_job_list_delete_job_use_yn_ajax, name='server_job_list_delete_job_use_yn_ajax'),
+    path('server_job_list_reload_left_ajax', views.server_job_list_reload_left_ajax, name='server_job_list_reload_left_ajax'),
 ]
--- process_check/views.py
+++ process_check/views.py
@@ -286,3 +286,283 @@ def server_list_update_svr_use_yn_ajax(request):
     }
 
     return render(request, 'process_check/server_list_dummy_ajax.html', context)
+
+
+def server_job_list(request):
+    s_query = "SELECT COUNT(*) AS cnt FROM tmon_dba.job_info"
+    with connections['tmon_dba'].cursor() as cursor:
+        cursor.execute(s_query)
+        row = cursor.fetchone()
+
+    context = {
+        'job_count': row[0],
+    }
+
+    return render(request, 'server_job_list.html', context)
+
+def server_job_list_left_ajax(request):
+    if request.method == 'POST':
+        s_job_name = request.POST.get('s_job_name')
+
+        # print("-------------------------------------------------------------")
+        # print(s_job_name)
+
+        if s_job_name is None:
+            s_job_name=''
+
+        # 잡 리스트 및 JOB 스케줄 가져오기
+        s_query = "/*left*/SELECT ji.job_info_name, COUNT(svr) AS svr_total, IFNULL(SUM(use_yn),0) AS svr_use_total" + \
+                    " FROM server_list sl" + \
+                    " INNER JOIN job_server_map AS jsm ON sl.server_list_seqno = jsm.server_list_seqno" + \
+                    " RIGHT OUTER JOIN job_info AS ji ON jsm.job_info_seqno = ji.job_info_seqno" + \
+                    " where ji.job_info_name like '%" + s_job_name + "%'" + \
+                    " GROUP BY ji.job_info_name" + \
+                    " ORDER BY ji.job_info_name"
+        # print(s_query)
+        # print("-------------------------------------------------------------")
+
+        with connections['tmon_dba'].cursor() as cursor:
+            job_info_lists = []
+            cursor.execute(s_query)
+            job_info_lists = namedtuplefetchall(cursor)
+
+        if(len(job_info_lists) == 0): # 검색결과가 없다면
+            print("하나도없네")
+
+        context = {
+            'job_info_lists': job_info_lists,
+            's_job_name': s_job_name,
+        }
+
+        return render(request, 'server_job_list_left_ajax.html', context)
+
+    else:
+        return render(request, 'server_job_list_left_ajax.html')
+
+def server_job_list_right_ajax(request):
+    if request.method == 'POST':
+
+        job_info_names = request.POST.getlist('job_info_name[]') # 원래 입력값
+        job_info_name = ", ".join( repr(e) for e in job_info_names) # QUERY에 쓰일 JOB_NAME 값
+
+        s_svr = request.POST.get('s_svr') # 원래 입력값
+        checkbox_unregister = request.POST.get('checkbox_unregister') # 원래 입력값
+        checkbox_off = request.POST.get('checkbox_off') # 원래 입력값
+
+        # print("-------------------------------------------------------------")
+        # print("right POST 테스트")
+        # print("-------------------------------------------------------------")
+        # print(job_info_names)
+        # print("s_svr : " + str(s_svr))
+        # print("checkbox_unregister : " + str(checkbox_unregister))
+        # print("checkbox_off : " + str(checkbox_off))
+        # print("-------------------------------------------------------------")
+        if s_svr is None:
+            s_svr=''
+
+        if len(job_info_names) == 0:
+            # print("비어있는 값 입력")
+            job_svr_lists = ''
+            job_info_name = "''"
+
+        else:
+            job_svr_lists = []
+            for job_name in job_info_names:
+                if checkbox_off =='ON':
+                    str_checkbox_off = " AND use_yn=1"
+                else:
+                    str_checkbox_off = ""
+
+                # 미등록 안보기 ON
+                if checkbox_unregister == 'ON':
+                    s_query =   "/*right*/SELECT ji.job_info_name, REPLACE(sl.svr,'.tmonc.net','') AS svr, jsm.use_yn" + \
+                                " FROM server_list sl" + \
+                                " INNER JOIN job_server_map AS jsm ON sl.server_list_seqno = jsm.server_list_seqno" + \
+                                " RIGHT OUTER JOIN job_info AS ji ON jsm.job_info_seqno = ji.job_info_seqno" + \
+                                " WHERE 1=1" + \
+                                " AND sl.svr IS NOT NULL AND jsm.use_yn IS NOT NULL" + \
+                                " AND ji.job_info_name='" + job_name + "'" + \
+                                " AND sl.svr like '%" + s_svr + "%'" + \
+                                str_checkbox_off + \
+                                " ORDER BY ji.job_info_name, svr"
+
+                # 미등록 OFF
+                else:
+                    s_query =  "/*right*/SELECT ji.job_info_name, REPLACE(sl.svr,'.tmonc.net','') AS svr, jsm.use_yn" + \
+                                " FROM server_list sl JOIN job_info AS ji" + \
+                                " LEFT JOIN job_server_map AS jsm ON sl.server_list_seqno = jsm.server_list_seqno AND ji.job_info_seqno = jsm.job_info_seqno" + \
+                                " WHERE 1=1" + \
+                                " AND ji.job_info_name='" + job_name + "'" + \
+                                " AND sl.svr like '%" + s_svr + "%'" + \
+                               str_checkbox_off + \
+                               " ORDER BY ji.job_info_name, svr"
+
+                # print(s_query)
+                # print("-------------------------------------------------------------")
+
+                try:
+                    with connections['tmon_dba'].cursor() as cursor:
+                        cursor.execute(s_query)
+                        svr_lists = namedtuplefetchall(cursor)
+                        job_svr_lists.append([job_name, svr_lists])
+                finally:
+                    cursor.close()
+
+        # print(job_svr_lists)
+        context = {
+            'job_svr_lists': job_svr_lists,
+            'job_info_name': job_info_name,
+        }
+
+        return render(request, 'server_job_list_right_ajax.html', context)
+    else:
+        return render(request, 'server_job_list_right_ajax.html')
+
+def server_job_list_update_job_use_yn_ajax(request):
+    if request.method == 'POST':
+        job_name = request.POST.get('job_name')
+        svr = request.POST.get('svr')
+        flag = request.POST.get('flag') # true or false
+        use_yn = request.POST.get('use_yn') # None 일경우, 미등록 처리 하기 위함
+
+        flag = 1 if flag == 'true' else 0 # true = 1, false = 0
+        # print("------------------------------------------------------------------------------------------------------")
+        # print("/* use yn 입력값 테스트 */")
+        # print(job_name)
+        # print(svr)
+        # print("변경값 : " + str(flag))
+        # print("사용여부 : " + str(use_yn))
+        # print("------------------------------------------------------------------------------------------------------")
+
+        # 미등록 서버,잡 입력받는경우. INSERT
+        if use_yn == 'None':
+            print("미등록입니다. 신규 등록합니다.")
+            query =   "INSERT INTO job_server_map (job_info_seqno, server_list_seqno, use_yn)" + \
+                        " SELECT ji.job_info_seqno, sl.server_list_seqno, 1" + \
+                        " FROM server_list sl JOIN job_info AS ji" + \
+                        " WHERE 1=1" + \
+                        " AND ji.job_info_name='" + job_name + "'" + \
+                        " AND sl.svr='" + svr + ".tmonc.net'"
+
+        # 기 등록 서버, 잡 입력받은경우
+        else:
+            print("등록입니다. 업데이트합니다.")
+            query = "/*update_job_use_yn_ajax*/UPDATE job_server_map jsm SET use_yn=" + str(flag) + \
+                    " WHERE 1=1" + \
+                    " AND jsm.job_info_seqno = (SELECT job_info_seqno FROM job_info ji WHERE ji.job_info_name = '" + job_name+ "')" + \
+                    " AND jsm.server_list_seqno = (SELECT server_list_seqno FROM server_list sl WHERE sl.svr=CONCAT('" + svr + "','.tmonc.net'))"
+
+        # print("------------------------------------------------------------------------------------------------------")
+        # print(query)
+        # print("------------------------------------------------------------------------------------------------------")
+
+        try:
+            cursor = connections['tmon_dba'].cursor()
+            cursor.execute(query)
+            connection.commit()
+        except:
+            connection.rollback()
+        finally:
+            cursor.close()
+
+    context = {
+        'job_name': job_name,
+        'svr': svr,
+        'flag': flag,
+    }
+
+    return render(request, 'server_job_list_dummy_ajax.html', context)
+
+def server_job_list_delete_job_use_yn_ajax(request):
+    if request.method == 'POST':
+        job_name = request.POST.get('job_name')
+        svr = request.POST.get('svr')
+        flag = request.POST.get('flag') # true or false
+        use_yn = request.POST.get('use_yn') # None 일경우, 미등록 처리 하기 위함
+
+        flag = 1 if flag == 'true' else 0 # true = 1, false = 0
+        print("-------------------------------------------------------------------------------------------------------")
+        print("/* delete_job_use_yn_ajax 입력값 테스트 */")
+        print(job_name)
+        print(svr)
+        print("변경값 : " + str(flag))
+        print("사용여부 : " + str(use_yn))
+        print("-------------------------------------------------------------------------------------------------------")
+
+        # 등록잡 삭제하기
+        print("등록된 잡 삭제하기")
+        query = "/*delete_job_use_yn_ajax*/ DELETE FROM job_server_map" + \
+                " WHERE 1=1" + \
+                " AND job_info_seqno = (SELECT job_info_seqno FROM job_info ji WHERE ji.job_info_name = '" + job_name+ "')" + \
+                " AND server_list_seqno = (SELECT server_list_seqno FROM server_list sl WHERE sl.svr=CONCAT('" + svr + "','.tmonc.net'))"
+        # print("------------------------------------------------------------------------------------------------------")
+        print(query)
+        print("-------------------------------------------------------------------------------------------------------")
+
+        try:
+            cursor = connections['tmon_dba'].cursor()
+            cursor.execute(query)
+            connection.commit()
+        except:
+            connection.rollback()
+        finally:
+            cursor.close()
+
+    context = {
+        'job_name': job_name,
+        'svr': svr,
+        'flag': flag,
+    }
+
+    return render(request, 'server_job_list_dummy_ajax.html', context)
+
+def server_job_list_reload_left_ajax(request):
+    if request.method == 'POST':
+        time.sleep(0.2)
+        job_info_name = request.POST.getlist('job_info_name[]')
+        s_job_name = request.POST.get('s_job_name')
+        s_svr = request.POST.get('s_svr')
+        checkbox_unregister = request.POST.get('checkbox_unregister')
+        checkbox_off = request.POST.get('checkbox_off')
+
+        # print("-------------------------------------------------------------")
+        # print("server_job_list_reload_left_ajax")
+        # print("-------------------------------------------------------------")
+        # print(s_job_name)
+        # print(job_info_name)
+        # print(s_svr)
+        # print(checkbox_unregister)
+        # print(checkbox_off)
+        # print("-------------------------------------------------------------")
+
+        if s_job_name is None:
+            s_job_name=''
+
+        # 잡 리스트 및 JOB 스케줄 가져오기
+        s_query = "/*left-reload*/SELECT ji.job_info_name, COUNT(svr) AS svr_total, IFNULL(SUM(use_yn),0) AS svr_use_total" + \
+                    " FROM server_list sl" + \
+                    " INNER JOIN job_server_map AS jsm ON sl.server_list_seqno = jsm.server_list_seqno" + \
+                    " RIGHT OUTER JOIN job_info AS ji ON jsm.job_info_seqno = ji.job_info_seqno" + \
+                    " where ji.job_info_name like '%" + s_job_name + "%'" + \
+                    " GROUP BY ji.job_info_name" + \
+                    " ORDER BY ji.job_info_name"
+        # print(s_query)
+
+        # print("-------------------------------------------------------------")
+        with connections['tmon_dba'].cursor() as cursor:
+            job_info_lists = []
+            cursor.execute(s_query)
+            job_info_lists = namedtuplefetchall(cursor)
+
+        context = {
+            'job_info_lists': job_info_lists,
+            'job_info_name_checked_list': job_info_name,
+            's_svr': s_svr,
+            'checkbox_unregister': checkbox_unregister,
+            'checkbox_off': checkbox_off,
+        }
+
+        return render(request, 'server_job_list_left_ajax.html', context)
+
+    else:
+        return render(request, 'server_job_list_left_ajax.html')
